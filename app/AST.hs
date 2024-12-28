765ed4c46bccdb964da128e95401e49bb64eff6c
James Ma <jamesma100@gmail.com>
2024-12-28 13:59:08 -0800

    add ast interpreter logic

--- app/AST.hs
+++ app/AST.hs
@@ -1,54 +1,145 @@
+module AST where
+import Data.Fixed
+
 data Node =
-  Number Float |
-  X |
-  Y |
-  Add Node Node |
-  Mult Node Node |
-  Triple Node Node Node |
-  Null
+  NumberNode Double |
+  BoolNode Bool |
+  XNode |
+  YNode |
+  AddNode Node Node |
+  MultNode Node Node |
+  ModNode Node Node |
+  TripleNode Node Node Node |
+  GTNode Node Node |
+  GTENode Node Node |
+  LTNode Node Node |
+  LTENode Node Node |
+  IfNode Node Node Node |
+  NormNode Node |
+  NullNode
   deriving Show
 
-node_eval :: Node -> Float -> Float -> Node
-node_eval (Number val) _ _ = Number val
-node_eval X x _ = Number x
-node_eval Y _ y = Number y
-node_eval (Add lhs rhs) x y =
-  case ((node_eval lhs x y), (node_eval rhs x y)) of
-    (Number first, Number second) -> Number (first + second)
-    (_, _) -> Null
-node_eval (Mult lhs rhs) x y =
-  case ((node_eval lhs x y), (node_eval rhs x y)) of
-    (Number first, Number second) -> Number (first * second)
-    (_, _) -> Null
-node_eval (Triple first second third) x y =
-  case (node_eval first x y, node_eval second x y, node_eval third x y) of
-    (Number a, Number b, Number c) -> Triple (Number a) (Number b) (Number c)
-    (_, _, _) -> Null
-node_eval Null _ _ = Null
+nodeEval :: Node -> Double -> Double -> Node
+nodeEval (NumberNode val) _ _ = NumberNode val
+nodeEval (BoolNode val) _ _ = BoolNode val
+nodeEval XNode x _ = NumberNode x
+nodeEval YNode _ y = NumberNode y
+nodeEval (AddNode lhs rhs) x y =
+  case ((nodeEval lhs x y), (nodeEval rhs x y)) of
+    (NumberNode first, NumberNode second) -> NumberNode (first + second)
+    (_, _) -> NullNode
+nodeEval (MultNode lhs rhs) x y =
+  case ((nodeEval lhs x y), (nodeEval rhs x y)) of
+    (NumberNode first, NumberNode second) -> NumberNode (first * second)
+    (_, _) -> NullNode
+nodeEval (ModNode lhs rhs) x y =
+  case (nodeEval lhs x y, nodeEval rhs x y) of
+    (NumberNode first, NumberNode second) -> NumberNode (mod' first second)
+    (_, _) -> NullNode
+nodeEval (TripleNode first second third) x y =
+  case (nodeEval first x y, nodeEval second x y, nodeEval third x y) of
+    (NumberNode a, NumberNode b, NumberNode c) ->
+      TripleNode (NumberNode a) (NumberNode b) (NumberNode c)
+    (_, _, _) -> NullNode
+nodeEval (GTNode lhs rhs) x y = 
+  case (nodeEval lhs x y, nodeEval rhs x y) of
+    (NumberNode a, NumberNode b) ->
+      if a > b then BoolNode True
+      else BoolNode False
+    (_, _) -> NullNode
+nodeEval (GTENode lhs rhs) x y = 
+  case (nodeEval lhs x y, nodeEval rhs x y) of
+    (NumberNode a, NumberNode b) ->
+      if a >= b then BoolNode True
+      else BoolNode False
+    (_, _) -> NullNode
+nodeEval (LTNode lhs rhs) x y = 
+  case (nodeEval lhs x y, nodeEval rhs x y) of
+    (NumberNode a, NumberNode b) ->
+      if a < b then BoolNode True
+      else BoolNode False
+    (_, _) -> NullNode
+nodeEval (LTENode lhs rhs) x y = 
+  case (nodeEval lhs x y, nodeEval rhs x y) of
+    (NumberNode a, NumberNode b) ->
+      if a <= b then BoolNode True
+      else BoolNode False
+    (_, _) -> NullNode
+nodeEval (IfNode condExpr thenExpr elseExpr) x y =
+  case (nodeEval condExpr x y, nodeEval thenExpr x y, nodeEval elseExpr x y) of
+    (BoolNode condVal, TripleNode a b c, TripleNode i j k) ->
+      if condVal then (TripleNode a b c)
+      else (TripleNode i j k)
+    (_, _, _) -> NullNode
+--normalizes [-1, 1] -> [0, 1]
+nodeEval (NormNode node) x y =
+  case (nodeEval node x y) of
+    (NumberNode val) -> (NumberNode ((val + 1) / 2.0))
+    _ -> NullNode
+nodeEval NullNode _ _ = NullNode
 
-node_print :: Node -> String
-node_print (Number val) = show val
-node_print X = "X"
-node_print Y = "Y"
-node_print (Add lhs rhs) =
+nodeGet :: Node -> (Double, Double, Double)
+nodeGet (TripleNode first second third) =
+  case (first, second, third) of
+    (NumberNode firstVal, NumberNode secondVal, NumberNode thirdVal) ->
+      (firstVal, secondVal, thirdVal)
+    (_, _, _) -> (0.0, 0.0, 0.0)
+
+nodePrint :: Node -> String
+nodePrint (NumberNode val) = show val
+nodePrint (BoolNode val) = show val
+nodePrint XNode = "X"
+nodePrint YNode = "Y"
+nodePrint (AddNode lhs rhs) =
   "add(" ++
-  (node_print lhs) ++
-  "," ++
-  (node_print rhs)
+  (nodePrint lhs) ++
+  ", " ++
+  (nodePrint rhs)
   ++
   ")"
-node_print (Mult lhs rhs) =
+nodePrint (MultNode lhs rhs) =
   "mult(" ++
-  (node_print lhs) ++
-  "," ++
-  (node_print rhs) ++
+  (nodePrint lhs) ++
+  ", " ++
+  (nodePrint rhs) ++
   ")"
-node_print (Triple first second third) =
+nodePrint (ModNode lhs rhs) =
+  (nodePrint lhs) ++ 
+  " % " ++
+  (nodePrint rhs)
+nodePrint (TripleNode first second third) =
   "(" ++
-  (node_print first) ++
-  "," ++
-  (node_print second) ++
-  "," ++
-  (node_print third) ++
+  (nodePrint first) ++
+  ", " ++
+  (nodePrint second) ++
+  ", " ++
+  (nodePrint third) ++
   ")"
-node_print Null = "NULL"
+nodePrint (GTNode lhs rhs) =
+  (nodePrint lhs) ++
+  " > " ++
+  (nodePrint rhs)
+nodePrint (GTENode lhs rhs) =
+  (nodePrint lhs) ++
+  " >= " ++
+  (nodePrint rhs)
+nodePrint (LTNode lhs rhs) =
+  (nodePrint lhs) ++
+  " < " ++
+  (nodePrint rhs)
+nodePrint (LTENode lhs rhs) =
+  (nodePrint lhs) ++
+  " < " ++
+  (nodePrint rhs)
+nodePrint (IfNode cond thenVal elseVal) =
+  "if (" ++
+  (nodePrint cond) ++
+  ") then " ++
+  (nodePrint thenVal) ++
+  " else " ++
+  (nodePrint elseVal)
+nodePrint (NormNode node) =
+  "norm( " ++
+  (nodePrint node) ++
+  ")"
+nodePrint NullNode = "NULL"
--- app/Render.hs
+++ app/Render.hs
@@ -1,3 +1,5 @@
+module Render where
+
 import Prelude as P
 import Graphics.Image as I
 import Data.Fixed
@@ -9,14 +11,20 @@ import Data.Fixed
 -- is done by `mapOnto`.
 --
 -- Example usage:
--- let colorImage = createImage 200 colorGrad
--- writeImage "../images/color.png" colorImage
+-- ghci> let grayImage = createImage 200 grayGrad
+-- ghci> writeImage "../images/gray.png" grayImage
+--
+-- Alternatively, instead of passing in a function, you can interpret an AST node.
+-- ghci> let grayGradAST = TripleNode (MultNode (AddNode XNode (NumberNode 1.0)) (NumberNode 0.5)) (MultNode (AddNode XNode (NumberNode 1.0)) (NumberNode 0.5)) (MultNode (AddNode XNode (NumberNode 1.0)) (NumberNode 0.5))
+-- ghci> evalGrayGradAST (x, y) = nodeGet (nodeEval grayGradAST x y)
+-- let grayImage = createImage 200 evalGrayGradAST
+-- ghci> writeImage "../images/gray.png" grayImage
 
--- normalizes pixel input [0, dim]^2 -> [-1, 1]^2
+-- normalizes pixel input [0, dim]^2 -> [-1, 1]^2 and flips (row, col) -> (x, y)
 mapOnto :: Int -> (Int, Int) -> (Double, Double)
-mapOnto dim (x, y) = (
-  ((fromIntegral x) - halfDim) / halfDim,
-  ((fromIntegral y) - halfDim) / halfDim)
+mapOnto dim (r, c) = (
+  ((fromIntegral c) - halfDim) / halfDim,
+  ((fromIntegral r) - halfDim) / halfDim)
   where halfDim = fromIntegral dim / 2.0
 
 -- example function: gray gradient, normalizes output from [-1, 1]^3 to [0, 1]^3
@@ -26,13 +34,17 @@ grayGrad (x, y) = ((x + 1) / 2.0, (x + 1) / 2.0, (x + 1) / 2.0)
 -- example function: color gradient, normalizes output from [-1, 1]^3 to [0, 1]^3
 colorGrad :: (Double, Double) -> (Double, Double, Double)
 colorGrad (x, y)
-  | x*y > 0 = ((x + 1) / 2.0, (y + 1) / 2.0, 1.0)
-  | y == 0 = (0.0, 0.0, 0.0) -- prevent divide by zero
+  | x*y >= 0 = ((x + 1) / 2.0, (y + 1) / 2.0, 1.0)
   | otherwise = (((mod' x y) + 1) / 2.0, ((mod' x y) + 1) / 2.0, ((mod' x y) + 1) / 2.0)
 
 -- creates RGB pixel, takes in function :: [-1, 1]^2 -> [0, 1]^3
 mkPixel :: (Double, Double, Double) -> Pixel RGB Double
 mkPixel (x, y, z) = PixelRGB x y z
 
+createImage0 :: Int -> ((Double, Double) -> (Double, Double, Double)) -> Image VU RGB Double
+createImage0 dim func = makeImageR VU (dim, dim) (mkPixel . func . mapOnto dim)
+
 createImage :: Int -> ((Double, Double) -> (Double, Double, Double)) -> Image VU RGB Double
-createImage dim func = makeImageR VU (dim, dim) (mkPixel . func . mapOnto dim)
+createImage dim func = fromLists [
+  [(mkPixel . func . mapOnto dim) (i, j) | j <- [1..dim+1]]
+  | i <- [1..dim+1]]
