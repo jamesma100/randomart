94cb9b52b8a01dec54f94689e0d8bebaf339f9ff
James Ma <jamesma100@gmail.com>
2025-01-04 21:03:33 -0800

    attempt 1 to implement algorithm in paper

--- app/AST.hs
+++ app/AST.hs
@@ -1,29 +1,107 @@
 module AST where
 import Data.Fixed
+import Data.List
+import System.Random
+import Control.Monad.Random
 
 data Node =
-  NumberNode Double |
-  BoolNode Bool |
-  XNode |
-  YNode |
-  AddNode Node Node |
-  MultNode Node Node |
-  ModNode Node Node |
+  NumberNode Double         |
+  BoolNode Bool             |
+  XNode                     |
+  YNode                     |
+  AddNode Node Node         |
+  MultNode Node Node        |
+  ModNode Node Node         |
   TripleNode Node Node Node |
-  GTNode Node Node |
-  GTENode Node Node |
-  LTNode Node Node |
-  LTENode Node Node |
-  IfNode Node Node Node |
-  NormNode Node |
-  NullNode
+  GTNode Node Node          |
+  GTENode Node Node         |
+  LTNode Node Node          |
+  LTENode Node Node         |
+  IfNode Node Node Node     |
+  NormNode Node             |
+  NullNode                  |
+  RuleNode [Node] Int       
   deriving Show
+
+-- TODO: don't hardcode this
+isTerminal :: Node -> Bool
+isTerminal node = case node of
+  RuleNode rules _ -> False
+  NumberNode _ -> True
+  BoolNode _ -> True
+  XNode -> True
+  YNode -> True
+  NullNode -> True
+  otherwise        -> case (elem False [isTerminal child | child <- getRules node]) of
+    True -> False
+    _    -> True
+
+getArity :: Node -> Int
+getArity node = case node of
+  RuleNode rules _ -> length rules
+  otherwise        -> 0
+
+-- get rules from a RuleNode
+getRules :: Node -> [Node]
+getRules node = case node of
+  (RuleNode rules _) -> rules
+  _                  -> []
+
+-- maps value [0,1] to [0, index]
+randRange :: Int -> Double -> Int
+randRange maxVal randVal = round $ randVal * (fromIntegral maxVal)
+
+type Grammar = [Node]
+
+-- grammar G = [r1,..., rn]
+-- initial rule i
+-- depth d
+-- list of random numbers randNums
+--
+-- - Random number is selected based on depth
+--
+-- Example:
+-- ghci> let ruleA = RuleNode [XNode, YNode]
+-- ghci> let ruleC = RuleNode [ruleA, AddNode ruleC ruleC, MultNode ruleC ruleC]
+-- ghci> let ruleE = [TripleNode ruleC ruleC ruleC]
+--
+-- non-terminal node: RuleNode [RuleNode [XNode, YNode] 2] 1
+treeGen :: Grammar -> Node -> Int -> [Double] -> Node
+treeGen grammar initialRule depth randNums
+  | depth <= 0 =
+    case () of
+      () | isTerminal firstRule -> firstRule 
+         | otherwise            -> firstRule -- TODO
+         where firstRule = (getRules initialRule) !! 0
+  | otherwise =
+    case () of
+      () | isTerminal curNode -> curNode
+         | otherwise          -> case curNode of
+          AddNode first second -> AddNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          MultNode first second -> MultNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          ModNode first second -> ModNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          TripleNode first second third ->
+            TripleNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums) (treeGen grammar third (depth-1) randNums)
+          GTNode first second -> GTNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          GTENode first second -> GTENode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          LTNode first second -> LTNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          LTENode first second -> LTENode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums)
+          IfNode first second third ->
+            IfNode (treeGen grammar first (depth-1) randNums) (treeGen grammar second (depth-1) randNums) (treeGen grammar third (depth-1) randNums)
+          _ -> NullNode
+         where curNode = (getRules initialRule) !! (randRange arity (randNums !! depth))
+         where arity = getArity initialRule
+      
+  
+-- randomList :: Int -> Int -> [Int]
+-- randomList seed sz = randomRs (0, sz) (mkStdGen seed) :: [Int]
 
 nodeEval :: Node -> Double -> Double -> Node
 nodeEval (NumberNode val) _ _ = NumberNode val
 nodeEval (BoolNode val) _ _ = BoolNode val
 nodeEval XNode x _ = NumberNode x
 nodeEval YNode _ y = NumberNode y
+--nodeEval (RandNode seed) _ _ = NumberNode (head (take 1 (randomList seed)))
 nodeEval (AddNode lhs rhs) x y =
   case ((nodeEval lhs x y), (nodeEval rhs x y)) of
     (NumberNode first, NumberNode second) -> NumberNode (first + second)
@@ -77,6 +155,9 @@ nodeEval (NormNode node) x y =
     (NumberNode val) -> (NumberNode ((val + 1) / 2.0))
     _ -> NullNode
 nodeEval NullNode _ _ = NullNode
+nodeEval (RuleNode rules idx) x y =
+  nodeEval (rules!!idx) x y
+  
 
 nodeGet :: Node -> (Double, Double, Double)
 nodeGet (TripleNode first second third) =
@@ -91,55 +172,56 @@ nodePrint (BoolNode val) = show val
 nodePrint XNode = "X"
 nodePrint YNode = "Y"
 nodePrint (AddNode lhs rhs) =
-  "add(" ++
+  "add("          ++
   (nodePrint lhs) ++
-  ", " ++
-  (nodePrint rhs)
-  ++
+  ", "            ++
+  (nodePrint rhs) ++
   ")"
 nodePrint (MultNode lhs rhs) =
-  "mult(" ++
+  "mult("         ++
   (nodePrint lhs) ++
-  ", " ++
+  ", "            ++
   (nodePrint rhs) ++
   ")"
 nodePrint (ModNode lhs rhs) =
   (nodePrint lhs) ++ 
-  " % " ++
+  " % "           ++
   (nodePrint rhs)
 nodePrint (TripleNode first second third) =
-  "(" ++
-  (nodePrint first) ++
-  ", " ++
+  "("                ++
+  (nodePrint first)  ++
+  ", "               ++
   (nodePrint second) ++
-  ", " ++
-  (nodePrint third) ++
+  ", "               ++
+  (nodePrint third)  ++
   ")"
 nodePrint (GTNode lhs rhs) =
   (nodePrint lhs) ++
-  " > " ++
+  " > "           ++
   (nodePrint rhs)
 nodePrint (GTENode lhs rhs) =
   (nodePrint lhs) ++
-  " >= " ++
+  " >= "          ++
   (nodePrint rhs)
 nodePrint (LTNode lhs rhs) =
   (nodePrint lhs) ++
-  " < " ++
+  " < "           ++
   (nodePrint rhs)
 nodePrint (LTENode lhs rhs) =
   (nodePrint lhs) ++
-  " < " ++
+  " < "           ++
   (nodePrint rhs)
 nodePrint (IfNode cond thenVal elseVal) =
-  "if (" ++
-  (nodePrint cond) ++
-  ") then " ++
+  "if ("              ++
+  (nodePrint cond)    ++
+  ") then "           ++
   (nodePrint thenVal) ++
-  " else " ++
+  " else "            ++
   (nodePrint elseVal)
 nodePrint (NormNode node) =
-  "norm( " ++
+  "norm( "         ++
   (nodePrint node) ++
   ")"
 nodePrint NullNode = "NULL"
+nodePrint (RuleNode rules idx) =
+  intercalate " | " [(nodePrint a) | a <- rules]
--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,25 @@
 module Main where
 
+--import Control.Monad.Random
 import Prelude as P
 import Graphics.Image as I
+import System.Random
+import AST
+import Render
 
+randList :: Int -> [Double]
+randList seed = randoms (mkStdGen seed) :: [Double]
+
+
+-- Ideas for getting random rule using IO Monad
+--
+-- 1. generate sizable list of random values beforehand [0, 1] - has to be done w/i IO monad
+-- 2. pass into type constructor, then
+--    a. select a random number from list
+--    b. use `randRange` to map to [0, arity-1], getting an index
+--    c. select grammar rule using index
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let seed = 420
+  let randNums = take 100 $ randList seed
+  putStrLn "hello world"
